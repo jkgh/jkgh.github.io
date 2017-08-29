3e585d1e8549454ec6fe7beaf0c7f5a3de80e973
Joanne Ho <joanneho@tw.ibm.com>
2017-08-29 12:10:57 +0800

    Image Nav

--- pages/mock/js/coupon.js
+++ pages/mock/js/coupon.js
@@ -1,13 +1,50 @@
-var openNav = function (anchorname) {
+var couponNameGlobal;
+var openNav = function () {
 	document.getElementById("myNav").style.width = "100%";
-	$('#couponIMG').attr("src", "images/" +anchorname + "_1.PNG");
+	$('#couponIMG').attr("src", "images/" + couponNameGlobal + "_1.PNG");
 }
 var closeNav = function () {
 	document.getElementById("myNav").style.width = "0%";
 }
+var nextNav = function(el){
+	console.log($(el).parent().find("img").attr("src"));
+	var currentImgSrc = $(el).parent().find("img").attr("src"); //images/big_mac_1.PNG
+	var tempCurrImgSrc = currentImgSrc;
+	tempCurrImgSrc = tempCurrImgSrc.replace("images/"+couponNameGlobal,'');
+	tempCurrImgSrc = tempCurrImgSrc.replace("_", "");
+	tempCurrImgSrc = tempCurrImgSrc.replace(".PNG", "");
+	console.log("tempCurrImgSrc: "+ tempCurrImgSrc);
+	if (tempCurrImgSrc == "1"){
+		$(el).parent().find("img").attr("src", "images/" + couponNameGlobal + "_2.PNG");
+	}else if (tempCurrImgSrc == "2"){
+		$(el).parent().find("img").attr("src", "images/" + couponNameGlobal + "_3.PNG");
+	}else if (tempCurrImgSrc == "3"){
+		//$(el).parent().find("img").attr("src", "images/" + couponNameGlobal + "_3.PNG");
+		//hide maybe?
+	}
+};
+
+var prevNav = function (el){
+	console.log($(el).parent().find("img").attr("src"));
+	var currentImgSrc = $(el).parent().find("img").attr("src"); //images/big_mac_1.PNG
+	var tempCurrImgSrc = currentImgSrc;
+	tempCurrImgSrc = tempCurrImgSrc.replace("images/"+couponNameGlobal,'');
+	tempCurrImgSrc = tempCurrImgSrc.replace("_", "");
+	tempCurrImgSrc = tempCurrImgSrc.replace(".PNG", "");
+	console.log("tempCurrImgSrc: "+ tempCurrImgSrc);
+	if (tempCurrImgSrc == "1"){
+		//$(el).parent().find("img").attr("src", "images/" + couponNameGlobal + "_1.PNG");
+		//hide maybe?
+	}else if (tempCurrImgSrc == "2"){
+		$(el).parent().find("img").attr("src", "images/" + couponNameGlobal + "_1.PNG");
+	}else if (tempCurrImgSrc == "3"){
+		$(el).parent().find("img").attr("src", "images/" + couponNameGlobal + "_2.PNG");
+	}
+};
 $('.showFullScreenImageAnchor').click(function(){
 	    //$("body").css("background-image","url('images/test.png')"); // Onclick of button the background image of body will be test here. Give the image path in url
-	    openNav($(this).attr('name'));
+	    couponNameGlobal = $(this).attr('name');
+	    openNav();
 	    console.log($(this).attr('name'));
 
 });
