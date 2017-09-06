188641fe61a6541b91b2080ba60543ca665234d9
Joanne Ho <joanneho@tw.ibm.com>
2017-09-06 10:14:49 +0800

    upload package?

--- pages/mock/imageUpload/js/main.js
+++ pages/mock/imageUpload/js/main.js
@@ -10,6 +10,109 @@ var config = {
 };
 var gitHub = new GitHub(config);
 
+
+var oldReadme;
+var newReadme;
+function getText(){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://jkgh.github.io/pages/mock/images/readme.txt', true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+               console.log(request.responseText);
+               oldReadme = request.responseText;
+                return request.responseText;
+            }
+        }
+    }
+}
+
+// var textFile = null,
+//   makeTextFile = function (text) {
+//     var data = new Blob([text], {type: 'text/plain'});
+
+//     // If we are replacing a previously generated file we need to
+//     // manually revoke the object URL to avoid memory leaks.
+//     if (textFile !== null) {
+//       window.URL.revokeObjectURL(textFile);
+//     }
+
+//     textFile = window.URL.createObjectURL(data);
+
+//     // returns a URL you can use as a href
+//     return textFile;
+//   };
+function uploadReadme(readmeFile, commitTitle) {
+   // Creates an array of Promises resolved when the content
+   // of the file provided is read successfully.
+   var files = [];
+   files[0] = readmeFile;
+   var filesPromises = [].map.call(files, readFile);
+
+   return Promise
+      .all(filesPromises)
+      .then(function(files) {
+         return files.reduce(
+
+            function(promise, file) {
+               return promise.then(function() {
+                  file.filename = "pages/mock/images/readme.txt";
+                  // Upload the file on GitHub
+                  return gitHub.saveFile({
+                     repository: gitHub.repository,
+                     branchName: config.branchName,
+                     filename: file.filename,//"pages/mock/images/"+
+                     content: file.content,
+                     commitTitle: commitTitle
+                  });
+               });
+            },
+            Promise.resolve()
+         );
+      });
+}
+function saveTextAsFile(newFilenameToAdd, commitTitle)
+{
+
+    var textToWrite = oldReadme +"\n"+ newFilenameToAdd;
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+      var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        //downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        newReadme = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        //downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        newReadme = window.URL.createObjectURL(textFileAsBlob);
+        //downloadLink.onclick = destroyClickedElement;
+        //downloadLink.style.display = "none";
+        //document.body.appendChild(downloadLink);
+    }
+     uploadReadme(newReadme, commitTitle)
+      .then(function() {
+         alert('Your readme file has been saved correctly.');
+      })
+      .catch(function(err) {
+         console.error(err);
+         alert('Readme upload error. Please, try again.');
+      });
+
+    //downloadLink.click();
+}
+
+
 /**
  * Reads the content of the file provided. Returns a promise whose resolved value is an object literal containing the
  * name (<code>filename</code> property) and the content (<code>content</code> property) of the file.
@@ -42,6 +145,9 @@ function readFile(file) {
       fileReader.readAsDataURL(file);
    });
 }
+
+
+
 
 /**
  * Save the files provided on the repository with the commit title specified. Each file will be saved with
@@ -108,3 +214,9 @@ document.querySelector('form').addEventListener('submit', function (event) {
          alert('Something went wrong. Please, try again.');
       });
 });
+
+getText();
+
+
+
+
